6a3e2401bf7e45c7fd10ff79284269b17662548f
Rodisi <brunomarques87@gmail.com>
2015-06-10 12:16:47 +0100

    some alterations ro event details

--- eventdetails.php
+++ eventdetails.php
@@ -39,9 +39,14 @@
 		   
 			
 			
-			var latlng = new google.maps.LatLng(lat,lon);
+			var latlng = new google.maps.LatLng(10,10);
 				
 				map.setCenter(latlng);
+				var marker = new google.maps.Marker({
+				position: latlng,
+				map: map,
+				title: 'Hello World!'
+				});
 				infowindow = new google.maps.InfoWindow();
 				});
 		
@@ -73,17 +78,32 @@
         <h3> UE </h3>
       </div>
       <div data-role="content" id="content">
-        <div data-role="collapsible">
+        <div >
 		<h3>Detalhes Evento</h3>
 		<p>Nome.</p>
-		<p>local.</p>
+		<p>local.   </p>
+		<p><a href="http://maps.google.com/maps?q=35.128061,-106.535561&ll=35.126517,-106.535131&z=17" data-role="button" data-inline="true" data-ajax="false">Mostrar em google maps</a></p>
 		<p>hora.</p>
 		<p>descriçâo.</p>
 		<p><a href="index.html" data-role="button" data-inline="true" data-ajax="false">Vou</a>
 		<a href="index.html" data-role="button" data-inline="true" data-theme="b" data-ajax="false">Não vou</a></p>
 		<p><a href="criarEvento.php" data-role="button" data-inline="true" data-ajax="false">Editar Evento</a></p>
+		<a href="#popupBasic" data-rel="popup" data-role="button" data-inline="true">Convidar</a>
+
+		<div data-role="popup" data-corners="true" id="popupBasic">
+			<form action="convida.php">
+			email:<br>
+			<input type="text" name="email" ">
+			<br>
+			mensagem:<br>
+			<input type="text" name="mensagem" ">
+			<br><br>
+			<input type="submit" data-role="button" value="Convidar">
+			</form>
 		</div>
-        <div id="map_canvas" style="height:90%"></div>
+		</div>
+        <div id="map_canvas" style="height:50%"></div>
+		
       </div>
       <div data-theme="a" data-role="footer" data-position="fixed">
         <h3> UE </h3>
