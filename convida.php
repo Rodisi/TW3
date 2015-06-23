9f3abcef38e68f3cd68db42f56942cfc386b9ea1
Rodisi <brunomarques87@gmail.com>
2015-06-23 17:29:10 +0100

    Some code for email
    Still not working

--- convida.php
+++ convida.php
@@ -13,10 +13,28 @@ if(isset($_SESSION['user_id'])){
 	$mensagem = filter_var($_POST['mensagem'],FILTER_SANITIZE_STRING);
 	
 	include 'config.php';
+	include ("iCal.php");
 	
-	$sql="INSERT INTO convite (eventid , email ,estado , mensagem) VALUES ('$eventid','$email','0','$mensagem')";
-	
+	$sql="INSERT INTO convite (eventid , email ,estado , mensagem) VALUES ('$eventid','$email','0','$mensagem' )";
 	$result=mysqli_query($link, $sql);
+	$conviteid= mysqli_insert_id($link);
+	
+	$sql="SELECT * FROM evento WHERE eventid= '$eventid' ";
+	$result=mysqli_query($link, $sql);
+	$row = mysqli_fetch_array($result);
+	$local =$row['local'];
+	$data = $row['data'];
+	$hora = $row['hora'];
+	$descricao =$row['descricao'];
+	$nomeEvento = $row['nome;'];
+	$combinedDT = date('Y-m-d H:i:s', strtotime("$data $hora"));
+	
+	$result2 =sendIcalEmail($email,$meeting_date,$nomeEvento,$descricao,$conviteid,$mensagem,$local);
+	if($result2) {
+		echo "Email sent successfully.";
+	} else {
+		echo "A problem occured sending email";
+	} 
 	
 	
 	
